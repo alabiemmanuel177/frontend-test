bdb53c7beffadc677b1b5ab67202145e4a44a3b3
Emmanuel Alabi <alabiemmanuel177@gmail.com>
2022-06-18 08:32:40 +0100

    Fixes on homepage

--- src/components/homecards/Homecards.jsx
+++ src/components/homecards/Homecards.jsx
@@ -39,6 +39,52 @@ export const Homecards = () => {
           </button>
         </div>
       </div>
+      <div className="ourschemes">
+        <h1>Our Schemes</h1>
+        <div className="our_schemes">
+          <div className="ourschemes_card">
+            <img
+              src="https://borgenproject.org/wp-content/uploads/23379499480_a8b69dc31e_o-705x470.jpg"
+              alt=""
+            />
+            <div className="txt">
+              <p>HEALTH</p>
+            </div>
+          </div>
+          <div className="ourschemes_card">
+            <img
+              src="https://media.istockphoto.com/photos/mortarboard-on-books-and-graduation-scroll-on-the-deskeducation-picture-id1328618978?b=1&k=20&m=1328618978&s=170667a&w=0&h=A6Gowdib9-P6wTU8AxLqwokH0vOFOrxlj62hrwd0yvo="
+              alt=""
+            />
+            <p>EDUCATION</p>
+          </div>
+          <div className="ourschemes_card">
+            <img
+              src="https://images.unsplash.com/photo-1636986905406-758b0e280f49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZW1wb3dlcm1lbnR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+              alt=""
+            />
+            <p>EMPOWERMENT</p>
+          </div>
+          <div className="ourschemes_card">
+            <img
+              src="https://www.un.org/ruleoflaw/wp-content/uploads/2015/04/humanitarian.jpg"
+              alt=""
+            />
+            <p>
+              HUMANITARIAN <br />
+              RELIEF
+            </p>
+          </div>
+        </div>
+        <button
+          id="scheme-btn"
+          type="button"
+          className="btn btn-primary btn-lg"
+          color="white !important"
+        >
+          Read More
+        </button>
+      </div>
       <Sponsor />
     </div>
   );
